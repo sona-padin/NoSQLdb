b8448b631d803dd4f0eb6708453018c1e27fc581
Sona <sonapadin@gmail.com>
2020-10-04 22:05:11 -0400

    Added some changes and commented out random test code

--- dynamoNoSQL.py
+++ dynamoNoSQL.py
@@ -1,21 +1,25 @@
 import boto3
 import csv
 
+#Sona Padinjarekutt
+#CS1660 
+#Assignment Three
+
 #Add your AWS user credentials to access the resources under your IAM account
 s3 = boto3.resource('s3',
-    aws_access_key_id='<your access key>',
-    aws_secret_access_key='<your secret access key>')
+    aws_access_key_id='<add your access key here>',
+    aws_secret_access_key='<add your secret key here>')
 
 #Try to create a new bucket called datacontui
 try:
     s3.create_bucket(Bucket='datacontui', CreateBucketConfiguration={'LocationConstraint' : 'us-west-2'})
     #Upload a file, 'cardinal1.jpg' into the newly created Bucket
-    s3.Object('datacontui', 'cardinal1.jpg').put(Body=open('.\cardina1.jpg', 'rb'))
+    #s3.Object('datacontui', 'cardinal1.jpg').put(Body=open('.\cardina1.jpg', 'rb'))
 except:
     print("this may already exist")
 
 #Test to see if I can add a picture of a cardinal
-s3.Object('datacontui', 'cardinal1.jpg').put(Body=open('C:\CS1660\AssignmentThree\cardinal1.jpg', 'rb'))
+#s3.Object('datacontui', 'cardinal1.jpg').put(Body=open('C:\CS1660\AssignmentThree\cardinal1.jpg', 'rb'))
 
 #Look at the newly created (or existing) datacontui bucket
 bucket = s3.Bucket("datacontui")
@@ -23,8 +27,8 @@ bucket = s3.Bucket("datacontui")
 #Enter AWS credentials to access the Dynamo DB resource
 dyndb = boto3.resource('dynamodb',
     region_name='us-west-2',
-    aws_access_key_id='<your access key>',
-    aws_secret_access_key='<your secret access key>'
+    aws_access_key_id='<add your access key here>',
+    aws_secret_access_key='<add your secret key here>'
 )
 
 #Try to create a new table called DataTable
@@ -74,11 +78,17 @@ with open('C:\CS1660\AssignmentThree\experiments.csv', newline='') as csvfile:
         table.put_item(Item = metadata_item)
 
 #Query to get all data from table
-response = table.get_item(
-    Key = {
-        'PartitionKey' : 'experiment1',
-        'RowKey': 'data1'
-    }
-)
+exp_num = ['1', '2', '2', '3', '3'] #Allows for easy string appending for partition keys
 
-print(response)
+for i in range(1,6):
+    a = 'experiment'+ exp_num[i-1] #i.e experiment1, experiment2
+    b = 'data' + str(i) #i.e data1, data2, data3
+    response = table.get_item(
+        Key = {
+            'PartitionKey' : a,
+            'RowKey': b
+        }
+    )
+    print(response)
+    print('------------------------------')
+    print('------------------------------')
